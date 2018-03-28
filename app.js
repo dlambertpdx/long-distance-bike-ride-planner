4eca6398e3f527faa4c97b6dc13fc8c8db56cb93
sanvean74 <dlambertpdx@gmail.com>
2018-03-27 19:51:10 -0700

    fixed flexbox issue

--- app.js
+++ app.js
@@ -3,15 +3,16 @@
 const setTotal = total => document.getElementById('total').innerHTML = total + ' miles';
 
 function initMap() {
-  var directionsService = new google.maps.DirectionsService;
-  var directionsDisplay = new google.maps.DirectionsRenderer({
+  const directionsService = new google.maps.DirectionsService;
+  const directionsDisplay = new google.maps.DirectionsRenderer({
     draggable: true,
     map: map,
     panel: document.getElementById('right-panel')
   });
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 14,
-    center: { lat: 45.5231, lng: -122.6765 } //Portland, OR
+    center: { lat: 45.5231, lng: -122.6765 },  //Portland, OR
+    disableDefaultUI: true
   });
 
   directionsDisplay.setMap(map);
